12bf3c9d05cc772cf8c4a215d6a425d6d60a8694
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-19 16:10:17 +0000

    Fix tests and rubocop offenses

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,36 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe 'users index action' do 
+RSpec.describe 'Users', type: :request do
+  describe 'users index action' do
+    before(:example) { get '/users' }
+
     it 'return a successful response for users#index' do
-      get users
       expect(response).to have_http_status(200)
     end
 
     it 'renders te correct template' do
-      get users
       expect(response).to render_template(:index)
     end
 
     it 'includes placeholder text' do
-      get users
       expect(response.body).to include('Here is a list of all users')
     end
   end
 
   describe 'users show action' do
+    before(:example) { get '/users/1' }
+
     it 'return a successful response for users#show' do
-      get user
       expect(response).to have_http_status(200)
     end
 
     it 'renders te correct template' do
-      get user
       expect(response).to render_template(:show)
     end
 
     it 'includes placeholder text' do
-      get user
       expect(response.body).to include('Here is the information of a given user id')
     end
   end
