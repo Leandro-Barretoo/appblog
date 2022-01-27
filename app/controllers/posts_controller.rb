b533db0c764000ad037b8d18f39f284925909926
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-27 15:37:39 +0000

    Fix rubocop offenses

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find_by_id(params[:user_id])
-    @posts = Post.all.where("author_id = ?", params[:user_id]).order(created_at: :desc)
+    @posts = Post.all.where('author_id = ?', params[:user_id]).order(created_at: :desc)
   end
 
   def show
