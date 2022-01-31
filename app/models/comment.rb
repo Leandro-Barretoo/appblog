e6166d3b481b2213a7b2dca3eefb4f65f5708809
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-31 16:09:30 +0000

    Fix associations andrubocop offenses

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, counter_cache: :commentsCounter
+
+  def comment_updater(value)
+    post.update(commentsCounter: value)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,8 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, counter_cache: :likesCounter
+
+  def like_updater(value)
+    post.update(likesCounter: value)
+  end
 end
