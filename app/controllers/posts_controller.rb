28bb93fdc0b688d8653c6d5610e337dc133ee758
Leandro-Barretoo <officialdevmi@gmail.com>
2022-02-04 16:27:24 +0000

    Add flash messages and fix n+1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by_id(params[:user_id])
-    @posts = Post.all.where('author_id = ?', params[:user_id]).order(created_at: :desc)
+    # @user = User.find_by_id(params[:user_id])
+    # @posts = Post.all.where('author_id = ?', params[:user_id]).order(created_at: :desc)
+    @user = User.includes(:posts).find(params[:user_id])
+    @posts = @user.posts.includes(:comments).order(created_at: :desc)
   end
 
   def show
-    @post = Post.find_by_id(params[:id])
-    @user = User.find_by_id(params[:user_id])
+    # @post = Post.find_by_id(params[:id])
+    # @user = User.find_by_id(params[:user_id])
+    @user = User.find(params[:user_id])
+    @post = @user.posts.includes(:comments).find(params[:id])
   end
 
   def new
@@ -16,11 +20,13 @@ class PostsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @post = @user.posts.new(post_params)
+    @post = @user.posts.new(title: post_params[:title], text: post_params[:text], commentsCounter: 0, likesCounter: 0)
 
     if @post.save
+      flash[:success] = 'Post created successfully'
       redirect_to user_posts_path
     else
+      flash.now[:error] = 'Something went wrong'
       render :new
     end
   end
