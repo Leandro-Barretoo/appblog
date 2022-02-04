67b074391ded97020e762280bf6079aad1824f66
Leandro-Barretoo <officialdevmi@gmail.com>
2022-02-04 16:03:39 +0000

    Add remainig user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate user' do
+    subject { User.new(name: 'Leo', photo: 'https://someimag.jpeg', bio: 'I am a tester', postCounter: 0) }
+    before { subject.save }
+
+    it 'name should exist' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts count should be integer' do
+      subject.postCounter = 3
+      expect(subject).to be_valid
+    end
+
+    it 'posts count is integer' do
+      subject.postCounter = 'num'
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts count is equal or higher than 0' do
+      subject.postCounter = -3
+      expect(subject).to_not be_valid
+    end
+
+    it 'is a valid user if correct parameters' do
+      expect(subject).to be_valid
+    end
+  end
 end
